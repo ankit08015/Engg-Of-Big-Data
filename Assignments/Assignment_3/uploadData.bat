ce77a1982cfa9e76844c9281dc8ec441e2d6f716
ankit08015 <yadav.ank@husky.neu.edu>
2019-06-11 12:58:22 -0400

    added assignment_3

--- Assignments/Assignment_3/uploadData.bat
+++ Assignments/Assignment_3/uploadData.bat
@@ -1,9 +1,5 @@
 @echo off
 
-CALL downloadFile.bat
-
-CALL unzip.bat
-
-FOR %%G IN (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) DO mongoimport --db assignment_4 --type csv --collection nyse_new --headerline --file C:\temp\unzip\NYSE\NYSE_daily_prices_%%G.csv
+FOR %%G IN (A B C D E F G H I J K L M N O P Q R S T U V W X Y Z) DO mongoimport --db assignment_4 --type csv --collection stocks --headerline --file C:\temp\unzip\NYSE\NYSE_daily_prices_%%G.csv
 
 pause
