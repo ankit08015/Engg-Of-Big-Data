12cea24879d9b41c8dc4753c6654d3f0252ddea9
ankit08015 <yadav.ank@husky.neu.edu>
2019-08-05 11:53:09 -0400

    added final project

---
+++ Final_Project/WindowsBatchFiles/unzipData.bat
@@ -0,0 +1,23 @@
+@echo off
+setlocal
+cd /d %~dp0
+FOR %%G IN (1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008) DO Call :UnZipFile "C:\flight-data\unzip" "C:\flight-data\data-%%G.csv.bz2"
+
+pause 
+
+exit /b
+
+:UnZipFile <ExtractTo> <newzipfile>
+set vbs="%temp%\_.vbs"
+if exist %vbs% del /f /q %vbs%
+>%vbs%  echo Set fso = CreateObject("Scripting.FileSystemObject")
+>>%vbs% echo If NOT fso.FolderExists(%1) Then
+>>%vbs% echo fso.CreateFolder(%1)
+>>%vbs% echo End If
+>>%vbs% echo set objShell = CreateObject("Shell.Application")
+>>%vbs% echo set FilesInZip=objShell.NameSpace(%2).items
+>>%vbs% echo objShell.NameSpace(%1).CopyHere(FilesInZip)
+>>%vbs% echo Set fso = Nothing
+>>%vbs% echo Set objShell = Nothing
+cscript //nologo %vbs%
+if exist %vbs% del /f /q %vbs%
